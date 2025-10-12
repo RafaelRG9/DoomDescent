d56b7bb3a0ae2cc904962cf559c8de1b73848745
Rafael Ramirez-Gaston <rramirezgaston@gmail.com>
2025-10-12 13:26:44 -0500

    feat(content): add level 6 and level 9 talent tiers for all classes
    - Populates the `GameData` class with a new set of unique talents for the Warrior, Rogue, and Mage that become available at level 6 and level 9.

--- GameData.cs
+++ GameData.cs
@@ -74,8 +74,21 @@ public class GameData
             new StatModifier("Thick Hide", "Who needs brains when you have brawn? and you have neither. Permanently increases Max Health by 15.", Rarity.Uncommon, "MaxHealth", 15, "null",  0),
             new StatModifier("Weapon Master", "You smash things harder now, woop!. Permanently increases Strength by 2.", Rarity.Uncommon, "Strength", 2, "null", 0),
             new StatModifier("Adrenaline Rush", "You feel less pain now, big deal. Permanently increases Dexterity by 1.", Rarity.Common, "Dexterity", 1, "null", 0)
+        },
+            // Level 6 Talents
+            [6] = new List<Modifier>
+        {
+            new StatModifier("Juggernaut", "Moar brawn, but this time, less brains. Permanently increases Max Health by 20 and reduces Intellect by 5.", Rarity.Uncommon, "MaxHealth", 20, "Intellect", 5),
+            new StatModifier("Berserker", "Who needs full health when you can go faster?. Permanently increases Dexterity by 2.", Rarity.Uncommon, "Dexterity", 2, "null", 0),
+            new StatModifier("Mighty Blow", "ME BIG STRONG, HIT THINGS HARDER!. Permanently increases Strength by 3.", Rarity.Epic, "Strength", 3, "null", 0)
+        },
+            // Level 9 Talents
+            [9] = new List<Modifier>
+        {
+            new StatModifier("Unstoppable", "You just won't die, won't you?. Permanently increases Max Health by 25.", Rarity.Epic, "MaxHealth", 25, "null", 0),
+            new StatModifier("Whirlwind", "Fan goes hurr durr. Permanently increases Dexterity by 3.", Rarity.Epic, "Dexterity", 3, "null", 0),
+            new StatModifier("Colossal Strength", "ME DA STRONGEST, NOT THE INTELLIGENTEST. Permanently increases Strength by 10, Intellect reduced by 2.", Rarity.Legendary, "Strength", 4, "Intellect", 2)
         }
-            // TODO: Add extra tiers
         };
 
         // --- ROGUE TALENTS ---
@@ -86,8 +99,22 @@ public class GameData
         {
             new StatModifier("Fleet Footed", "I AM SPEED!. Permanently increases Dexterity by 2.", Rarity.Uncommon, "Dexterity", 2, "null", 0),
             new StatModifier("Precise Strikes", "Your stab got stabbier!. Permanently increases Strength by 1.", Rarity.Common, "Strength", 1, "null", 0),
-            new StatModifier("Toughness", "You are not as feeble as you think. Permanently increases Max Health by 10.", Rarity.Common, "MaxHealth", 10, "null", 0)
-        }
+            new StatModifier("Toughness", "You are not as feeble as I thought. Permanently increases Max Health by 10.", Rarity.Common, "MaxHealth", 10, "null", 0)
+        },
+            // Level 6 Talents
+            [6] = new List<Modifier>
+            {
+            new StatModifier("Evasion", "You can't C me!. Permanently increases Dexterity by 3.", Rarity.Uncommon, "Dexterity", 3, "null", 0),
+            new StatModifier("Killer Instinct", "Kill all the things!. Permanently increases Strength by 2.", Rarity.Uncommon, "Strength", 2, "null", 0),
+            new StatModifier("Iron Will", "Really, you are going for a tanky build? whatever!. Permanently increases Max Health by 15.", Rarity.Common, "MaxHealth", 15, "null", 0)
+            },
+            // Level 9 Talents
+            [9] = new List<Modifier>
+            {
+            new StatModifier("Shadowstep", "Sneaky stabby!. Permanently increases Dexterity by 4.", Rarity.Epic, "Dexterity", 4, "null", 0),
+            new StatModifier("Assassinate", "Kill kill kill. Permanently increases Strength by 3.", Rarity.Epic, "Strength", 3, "null", 0),
+            new StatModifier("Stone Skin", "Why do you even pick me?. Permanently increases Max Health by 20.", Rarity.Epic, "MaxHealth", 20, "null", 0)
+            }
         };
 
         // --- MAGE TALENTS ---
@@ -97,8 +124,22 @@ public class GameData
             [3] = new List<Modifier>
         {
             new StatModifier("Arcane Intellect", "You are smarter than a fifth grader!. Permanently increases Intellect by 3.", Rarity.Uncommon, "Intellect", 3, "null", 0),
-            new StatModifier("Glass Cannon", "Do I really need to explain this?. +2 Intellect, -5 Max Health.", Rarity.Epic, "Intellect", 2, "Health", 5),
+            new StatModifier("Glass Cannon", "Do I really need to explain this?. +5 Intellect, -5 Max Health.", Rarity.Epic, "Intellect", 5, "MaxHealth", 5),
             new StatModifier("Stamina", "Basic white mage buff. Permanently increases Max Health by 12.", Rarity.Common, "MaxHealth", 12, "null", 0)
+        },
+            // Level 6 Talents for Mage
+            [6] = new List<Modifier>
+        {
+            new StatModifier("Elemental Mastery", "You are even smarter than a fifth grader!. Permanently increases Intellect by 5.", Rarity.Epic, "Intellect", 5, "null", 0),
+            new StatModifier("Mana Fountain", "You can cast more fireballs, how original!. Permanently increases Max Energy by 20.", Rarity.Uncommon, "MaxEnergy", 20, "null", 0),
+            new StatModifier("Quick Thinking", "You are quick as a magical fox. Permanently increases Dexterity by 2.", Rarity.Uncommon, "Dexterity", 2, "null", 0)
+        },
+            // Level 9 Talents for Mage
+            [9] = new List<Modifier>
+        {
+            new StatModifier("Archmage", "You are the smartest fifth grader ever!. Permanently increases Intellect by 5.", Rarity.Legendary, "Intellect", 5, "null", 0),
+            new StatModifier("Manaforged", "Your magical reserves are unlimited!, not really. Permanently increases Max Energy by 25.", Rarity.Epic, "MaxEnergy", 25, "null", 0),
+            new StatModifier("Glassier Cannon", "all brains and no brawn. Permanently increases Intellect by 10, decreases health by 10.", Rarity.Epic, "Intellect", 10, "MaxHealth", 10)
         }
         };
     }
